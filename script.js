2fc549be3e89882800af8af02cdd0fd2f480a704
yukinch <167289373+yukinch@users.noreply.github.com>
2024-06-05 14:45:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript
+//console.log('Hello world!');
+
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    auto: true,
+    speed: 1500,
+    pause: 4000,
+  });
+});
